43ac81cfba0a91b1cd1d97aa5be64e9152e919ff
Caleb Schmidt <cschmi14@gmail.com>
2023-02-28 12:23:53 -0500

    Done

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Corolla";
+            myCar.Year = 2018;
+
+            Car wifeCar= new Car();
+            wifeCar.Make = "Toyota";
+            wifeCar.Model = "Camry";
+            wifeCar.Year = 2019;
+
+            var carList = new List<Car>() { myCar, wifeCar};
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
         }
     }
 }
